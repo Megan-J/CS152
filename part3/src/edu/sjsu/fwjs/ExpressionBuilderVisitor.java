c27a4155f228ecb9340ef938b71e903175f118ec
ttmtu2003 <ttmtu2003@gmail.com>
2024-12-08 21:32:26 -0800

    some methods for part3

--- part3/src/edu/sjsu/fwjs/ExpressionBuilderVisitor.java
+++ part3/src/edu/sjsu/fwjs/ExpressionBuilderVisitor.java
@@ -16,6 +16,66 @@ public class ExpressionBuilderVisitor extends FeatherweightJavaScriptBaseVisitor
         }
         return listToSeqExp(stmts);
     }
+    @Override
+    public Expression visitPrint(FeatherweightJavaScriptParser.PrintContext ctx) {
+        Expression exp = visit(ctx.expr());
+        return new PrintExpr(exp);
+    }
+    
+    @Override
+    public Expression visitVarDecl(FeatherweightJavaScriptParser.VarDeclContext ctx) {
+        String varName = ctx.ID().getText();
+        Expression exp = visit(ctx.expr());
+        return new VarDeclExpr(varName, exp);
+    }
+
+    @Override
+    public Expression visitFuncDecl(FeatherweightJavaScriptParser.FuncDeclContext ctx) {
+        List<String> params = visitParams(ctx.params());
+        Expression body = visit(ctx.block());
+        return new FunctionDeclExpr(params, body);
+    }
+
+    @Override
+    public Expression visitFuncApp(FeatherweightJavaScriptParser.FuncAppContext ctx) {
+        Expression func = new VarExpr(ctx.ID().getText()); // Treat function name as variable reference
+        List<Expression> args = visitArgs(ctx.args());
+        return new FunctionAppExpr(func, args);
+    }
+
+    @Override
+    public Expression visitBool(FeatherweightJavaScriptParser.BoolContext ctx) {
+        boolean val = Boolean.parseBoolean(ctx.BOOL().getText());
+        return new ValueExpr(new BoolVal(val));
+    }
+  
+    @Override
+    public Expression visitAddSub(FeatherweightJavaScriptParser.AddSubContext ctx) {
+        Expression left = visit(ctx.expr(0));
+        Expression right = visit(ctx.expr(1));
+        Op op = ctx.op.getType() == FeatherweightJavaScriptParser.ADD ? Op.ADD : Op.SUBTRACT;
+        return new BinOpExpr(op, left, right);
+    }
+
+    @Override
+    public Expression visitMulDivMod(FeatherweightJavaScriptParser.MulDivModContext ctx) {
+        Expression e1 = visit(ctx.expr(0));
+        Expression e2 = visit(ctx.expr(1));
+        Op op;
+        if (ctx.op.getType() == FeatherweightJavaScriptParser.MUL) {
+            op = Op.MULTIPLY;
+        } else if (ctx.op.getType() == FeatherweightJavaScriptParser.DIV) {
+            op = Op.DIVIDE;
+        } else {
+            op = Op.MOD;
+        }
+        return new BinOpExpr(op, e1, e2);
+    }
+
+    @Override
+    public Expression visitEmptyStmt(FeatherweightJavaScriptParser.EmptyStmtContext ctx) {
+        return new ValueExpr(new NullVal());
+    }
 
     @Override
     public Expression visitBareExpr(FeatherweightJavaScriptParser.BareExprContext ctx) {
